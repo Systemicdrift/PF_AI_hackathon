b92d5141260febde9d07c1a5795f1eb02eabb0ce
theworksofvon <davontae.jackson@PFXMAC-PG34100X67.attlocal.net>
2023-03-15 18:36:11 -0500

    its done....

--- app/gpt.py
+++ app/gpt.py
@@ -1,14 +1,18 @@
 import openai
 
-fine_tuned_model = "curie:ft-personal-2023-03-15-17-51-00"
+fine_tuned_model = "davinci:ft-personal-2023-03-15-22-47-24"
 openai.api_key = "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"
 
 
 def chatGptConversation(conversation):
     response = openai.Completion.create(
-        model=fine_tuned_model, prompt=conversation)
+        model=fine_tuned_model, prompt=conversation, temperature=0.1)
 
-    print(response)
+    print(response.choices[0].text)
 
 
-chatGptConversation("what is authz?")
+if __name__ == '__main__':
+    while 1:
+        print("Ask me a question about Powerflex...")
+        query = input()
+        chatGptConversation(query)
